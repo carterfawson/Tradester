e18fec0f5c8eff964b801a6904739f5870f1412c
Carter Fawson <carteraaronfawson@gmail.com>
2016-08-23 22:42:13 -0500

    more WIP

--- mvp/models.py
+++ mvp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class customer(models.Model):
     custid = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=100)
     email = models.EmailField(max_length=245)
     address = models.CharField(max_length=50)
     city = models.CharField(max_length=20)
